45f907195759ed0778f0db3c6e797ac41301b4dc
Stoicism-Sympatheia <30344545+stoicism-sympathei@users.noreply.github.com>
2022-11-11 16:56:29 +0300

    splits a string at the logical

--- strtol_logic.c
+++ strtol_logic.c
@@ -45,3 +45,51 @@ int	stralloc(char *str, int i, char flag, cmd_lst_lst_t *llav)
 	add_cmd_lst(buf, &llav->head, flag);
 	return (1);
 }
+/**
+  * free_r - frees r if error
+  * @size: the current size of r
+  * @r: a list of strings
+  **/
+void	free_r(size_t size, char **r)
+{
+	while (size > 0)
+		free(r[--size]);
+	free(r);
+}
+
+/**
+ * strsplit - a function that splits a string at the logical \
+ operator "||" and "&&"
+ * @s: the string in which to find the words
+ * @llav: the head of the llav ll
+ *
+ * Return: 0 if error, 1 otherwise
+ **/
+int	strsplit(cmd_lst_lst_t *llav, char *s)
+{
+	int	i = 0;
+	size_t	size = 1;
+	char	flag = 0;
+
+	if (!s || !*s)
+		return (0);
+	while (s[i])
+	{
+		if ((s[i] == '|' && s[i + 1] == '|') || (s[i] == '&' && s[i + 1] == '&'))
+		{
+			if (stralloc(s, i, flag, llav) == 1)
+			{
+				flag = (s[i] == '|') ? '|' : '&';
+				++size;
+				s += i + 2;
+				i = -1;
+			}
+			else
+				return (0);
+		}
+		++i;
+	}
+	if (stralloc(s, i, flag, llav) == -1)
+		return (0);
+	return (1);
+}
